5270416986c1dfe5490af07262caf02f047f40f7
Marvin Pokroppa <marvin@tygaware.de>
2018-11-11 22:04:21 +0100

    Added Exercise2 and Excercise2Tests projects

---
+++ Excercise2/StringExercise.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise2
+{
+    public class StringExercise
+    {
+        /// <summary>
+        /// Zählt die Vorkommen des Buchstaben 'a' (klein) in einer Zeichenkette.
+        /// Beispiel:
+        /// Mit dem String "Als Programmierer hat man Spaß" würde die Methode den Wert 4 zurückgeben.
+        /// </summary>
+        /// <param name="str">Zeichenkette in der gezählt werden soll</param>
+        /// <returns>Anzahl der Vorkommen</returns>
+        public static int CountAs(string str)
+        {
+            //Dummy-Wert
+            return -1; //Entfernen und richtig implementieren
+        }
+
+        /// <summary>
+        /// Zählt die Vorkommen der Buchstaben 'a','e' und 'c' (klein) in einer Zeichenkette.
+        /// Beispiel:
+        /// Mit dem String "Programmieren macht Spaß" würde die Methode den Wert 6 zurückgeben.
+        /// </summary>
+        /// <param name="str">Zeichenkette in der gezählt werden soll</param>
+        /// <returns>Anzahl der Vorkommen</returns>
+        public static int CountAsEsCs(string str)
+        {
+            //Dummy-Wert
+            return -1; //Entfernen und richtig implementieren
+        }
+
+        /// <summary>
+        /// Prüft, ob die Zeichenfolge "ab" (klein) in einer Zeichenketten vorhanden ist.
+        /// Beispiel: "ich habe einen schönen Abend" -> Gibt true zurück.
+        /// Beispiel: "das ist ein schöner Abend" -> Gibt false zurück.
+        /// </summary>
+        /// <param name="str">Zeichenkette, die geprüft werden soll</param>
+        /// <returns></returns>
+        public static bool IsABInString(string str)
+        {
+            //Dummy-Wert
+            return false; //Entfernen und richtig implementieren
+        }
+
+        /// <summary>
+        /// Spiegelt eine Zeichenkette und gibt diese zurück.
+        /// Beispiel: "Hallo" --> "ollaH"
+        /// </summary>
+        /// <param name="str">Zeichenkette, die gespiegelt werden soll</param>
+        /// <returns></returns>
+        public static string Reverse(string str)
+        {
+            //Dummy-Wert
+            return null; //Entfernen und richtig implementieren
+        }
+
+        /// <summary>
+        /// Prüft, ob eine übergeben Zahl (num) in der Zeichenkette vorhanden ist.
+        /// </summary>
+        /// <param name="str">Zeichenkette, in der gesucht werden soll</param>
+        /// <param name="num">Ganzzahl nach der gesucht werden soll</param>
+        /// <returns></returns>
+        public static bool IsNumInString(string str, int num)
+        {
+            //Dummy-Wert
+            return false; //Entfernen und richtig implementieren
+        }
+
+        /// <summary>
+        /// Schneidet eine Zeichenkette in 2 Teile und gibt einen Teil als Ergebnis zurück.
+        /// Wenn die Teile nicht gleichgroß sein sollten, soll der erste Teil der größere sein.
+        /// </summary>
+        /// <param name="str">Zeichenfolge, die geteilt werden soll.</param>
+        /// <param name="returnFirstPart">Wenn true, dann gebe den ersten Teil der Zeichenkette zurück; 
+        /// wenn false, den zweiten.</param>
+        /// <returns></returns>
+        public static string CutInHalf(string str, bool returnFirstPart)
+        {
+            //Dummy-Wert
+            return null; //Entfernen und richtig implementieren
+        }
+
+        /// <summary>
+        /// Zählt die Vorkommen einer beliebigen Zeichenfolge (word) in einem String (str).
+        /// Beispiel: CountStringInString("Programmierenistschwer ,aber,macht,Spaß,aber", ",abe") --> 2
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public static int CountStringInString(string str, string word)
+        {
+            //Dummy-Wert
+            return -1; //Entfernen und richtig implementieren
+        }
+    }
+}
---
+++ Excercise2Tests/StringExerciseTests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Excercise2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise2.Tests
+{
+    [TestClass()]
+    public class StringExerciseTests
+    {
+        [DataTestMethod()]
+        [DataRow("aahabe ich bin sein Abend du er sie esAa", 4)]
+        [DataRow("haben habe haben habe haben", 5)]
+        [DataRow("aaaaaaaaaa", 10)]
+        [DataRow("", 0)]
+        [DataRow("a", 1)]
+        [DataRow("AAAAAAAAAAAAAAAAhjgjzhgjhhrnbfkrfmoiurnjgnlknrgjktr   gtjghjkthgjkrt    ", 0)]
+        public void CountAsTest(string s, int expected)
+        {
+            int actual = StringExercise.CountAs(s);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod()]
+        [DataRow("Puroguramingu wa subarashii! Semento...c", 7)]
+        [DataRow("", 0)]
+        [DataRow("a", 1)]
+        [DataRow("e", 1)]
+        [DataRow("c", 1)]
+        [DataRow("ae", 2)]
+        [DataRow("ac", 2)]
+        [DataRow("aec", 3)]
+        [DataRow("aec eca cae eac", 12)]
+        public void CountAsEsCsTest(string s, int expected)
+        {
+            int actual = StringExercise.CountAsEsCs(s);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod()]
+        [DataRow("ab ich habe einen schönen abend ababbaab", true)]
+        [DataRow("ab", true)]
+        [DataRow("ab ab", true)]
+        [DataRow("enemenemuundrausbistdu", false)]
+        [DataRow("", false)]
+        [DataRow("a", false)]
+        [DataRow("b", false)]
+        [DataRow("a b", false)]
+        [DataRow("ba", false)]
+        public void IsABInStringTest(string s, bool expected)
+        {
+            bool actual = StringExercise.IsABInString(s);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod()]
+        [DataRow("HalloCSharp", "prahSCollaH")]
+        [DataRow("", "")]
+        [DataRow("a", "a")]
+        [DataRow("ab", "ba")]
+        [DataRow("abc", "cba")]
+        [DataRow("abc cba", "abc cba")]
+        public void ReverseTest(string s, string expected)
+        {
+            string actual = StringExercise.Reverse(s);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod()]
+        [DataRow("Ich bin i9ein 18String 19", 19, true)]
+        [DataRow("Ich bin i9ein 18String 18", 18, true)]
+        [DataRow("Ich bin i9ein 18String 19", 9, true)]
+        [DataRow("0", 0, true)]
+        [DataRow("Ich bin i9ein 18String 19", 11, false)]
+        [DataRow("1 1", 11, false)]
+        [DataRow("101", 11, false)]
+        public void IsNumInStringTest(string s, int num, bool expected)
+        {
+            bool actual = StringExercise.IsNumInString(s, num);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod()]
+        [DataRow("CutInHalf", true, "CutIn")]
+        [DataRow("CutInHalf", false, "Half")]
+        [DataRow("Maoam", true, "Mao")]
+        [DataRow("Maoam", false, "am")]
+        [DataRow("", false, "")]
+        [DataRow("", true, "")]
+        public void CutInHalfTest(string s, bool rf, string expected)
+        {
+            string actual = StringExercise.CutInHalf(s, rf);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod()]
+        [DataRow("ich habe bereits viel über c# gelernt. ichichich bin ich bin super!ich", "ich", 6)]
+        [DataRow("", "", 1)]
+        [DataRow("aaa aaa", "a", 6)]
+        [DataRow("aaa", "bbbbbb", 0)]
+        [DataRow("", "bbbbbb", 0)]
+        [DataRow("aaa ic", "ich", 0)]
+        [DataRow("ich ic", "ich", 1)]
+        [DataRow("aaa ich i", "ich", 1)]
+        public void CountStringInStringTest(string s, string word, int expected)
+        {
+            int actual = StringExercise.CountStringInString(s, word);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
